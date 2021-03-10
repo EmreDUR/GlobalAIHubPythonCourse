f64cabd45358ce1ec1816cd5e0c0ca83e8e81484
EmreDUR <80275530+EmreDUR@users.noreply.github.com>
2021-03-10 23:35:05 +0300

    Add files via upload

---
+++ Homeworks/homework2.py
@@ -0,0 +1,23 @@
+# - Homework 2 --> CV Application -
+
+# Create 5 dictionaries. Each dictionary should represent a CV.
+# Create a CV containing the information of the 5 created people.
+
+personOne = {"Name": "Ahmet,", "Surname": "Kaya",
+             "Age": 35, "City": "Ankara", "Job": "Engineer"}
+personTwo = {"Name": "Ayşe,", "Surname": "Taş",
+             "Age": 26, "City": "İstanbul", "Job": "Coder"}
+personThree = {"Name": "Mehmet,", "Surname": "Demir",
+               "Age": 40, "City": "İzmir", "Job": "Technician"}
+personFour = {"Name": "Fatma,", "Surname": "Çelik",
+              "Age": 37, "City": "Eskişehir", "Job": "Data Scientist"}
+personFive = {"Name": "Emre,", "Surname": "Dur",
+              "Age": 20, "City": "Elazığ", "Job": "Engineer"}
+
+# Print the information on the CVs created on the screen
+
+print(personOne)
+print(personTwo)
+print(personThree)
+print(personFour)
+print(personFive)
